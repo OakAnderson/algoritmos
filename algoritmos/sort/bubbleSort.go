faf1c12840724b8360d8d996b64ee6f3b6024f52
OakAnderson <andersonfelipe01@live.com>
2020-04-19 00:02:32 -0300

    Adicionado algoritmo bubble sort

---
+++ algoritmos/sort/bubbleSort.go
@@ -0,0 +1,39 @@
+// https://en.wikipedia.org/wiki/Bubble_sort
+// Performace no pior caso: O(n²)
+// Chamando BubbleSort(arr, true) será possível observar os processos de
+// ordenação
+package sort
+
+import "fmt"
+
+func BubbleSort(arr []int, simulation ...bool) []int {
+	swap := func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+
+	n := len(arr)
+	swapped := true
+
+	iteration := 0
+	sim := false
+	if len(simulation) > 0 && simulation[0] {
+		sim = true
+		fmt.Println("iteração", iteration, ":", arr)
+	}
+
+	for x := 0; swapped; x++ {
+		swapped = false
+		for i := 1; i < n-x; i++ {
+			if arr[i-1] > arr[i] {
+				swap(i-1, i)
+				swapped = true
+				if sim {
+					iteration++
+					fmt.Println("iteração", iteration, ":", arr)
+				}
+			}
+		}
+	}
+
+	return arr
+}
